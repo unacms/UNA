97dd3a119c1a00179f5ff85062e9939c4f573651
Aleksandr Trofimov <at@unacms.com>
2026-09-21 09:33:51 +0300

    Modules updates. SMTP mailer.

--- modules/boonex/smtpmailer/updates/15.0.0_15.0.1/install/sql/enable.sql
+++ modules/boonex/smtpmailer/updates/15.0.0_15.0.1/install/sql/enable.sql
@@ -8,8 +8,8 @@ INSERT INTO `sys_options_categories` (`type_id`, `name`, `caption`, `order` )
 VALUES (@iTypeId, 'bx_smtp_oauth', '_bx_smtp_adm_stg_cpt_category_oauth', 2);
 SET @iCategId = LAST_INSERT_ID();
 
-DELETE FROM `sys_options` WHERE `name` LIKE 'bx_smtp_oauth_%';
-INSERT INTO `sys_options` (`name`, `value`, `category_id`, `caption`, `type`, `check`, `check_error`, `order`, `extra`) VALUES
+UPDATE `sys_options` SET `category_id` = @iCategId WHERE `name` LIKE 'bx_smtp_oauth_%';
+INSERT IGNORE INTO `sys_options` (`name`, `value`, `category_id`, `caption`, `type`, `check`, `check_error`, `order`, `extra`) VALUES
 ('bx_smtp_oauth_on', '', @iCategId, '_bx_smtp_option_oauth_on', 'checkbox', '', '', 10, ''),
 ('bx_smtp_oauth_client_id', '', @iCategId, '_bx_smtp_oauth_client_id', 'digit', '', '', 20, ''),
 ('bx_smtp_oauth_tenant_id', '', @iCategId, '_bx_smtp_oauth_tenant_id', 'digit', '', '', 30, ''),
